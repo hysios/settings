6cc03253dd213ce4f26ede8b313893b53115cc4a
Hysios Hu <hysios@gmail.com>
2013-12-25 16:13:46 +0800

    add railtie.rb

---
+++ lib/railtie.rb
@@ -0,0 +1,10 @@
+module Settings
+  class Railtie < Rails::Railtie
+    initializer "settings.rails_load_initialization" do |app|
+      if defined?(Rails) && Rails.root
+        Settings.config_file Rails.root.join("/config/settings.yml")
+        Settings.load
+      end
+    end
+  end
+end
