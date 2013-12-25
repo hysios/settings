e73a0a283f8b98c64d16280d163cca6557799ff3
Hysios Hu <hysios@gmail.com>
2013-12-25 14:58:15 +0800

    final

--- lib/settings.rb
+++ lib/settings.rb
@@ -5,132 +5,154 @@ require "yaml"
 
 module Settings
 
-  class SettingsNode < Hash
+  module GenerateAccessorMethods
 
-
-    def method_missing(meth, *args, &block)
-
-      meth = meth.to_s
-      eigenclass = class << self; self; end
-
-      if meth[-1] == '=' # write
-        eigenclass.class_eval do
-          define_method(meth) do |value|
-            self[meth.slice(0..-2)] = value
-          end
-        end
-      else
-        eigenclass.class_eval do
-          define_method(meth) do |*args|
-            value = args[0]
-            if self.has_key?(meth)
-              if self[meth].is_a?(Hash)
-                SettingsNode[self[meth]]
-              else
-                self[meth]
-              end
-            else
-              self[meth] = value || SettingsNode.new
-            end
-          end
-        end
-      end
-      send(meth, *args, &block)     
+    def GenerateAccessorMethods.included(klass)
+      klass.extend(ClassMethods)
+      klass.class_variable_set(:@@target, nil)
     end
 
-    def generate_method(meth, value)
+    def generate_accessor_methods(method)
+      generate_writer_method(method)
+      generate_reader_method(method)
+    end
+
+    def generate_writer_method(method)
+      eigenclass = class << self; self; end
+      meth = method + '='
       eigenclass.class_eval do
         define_method(meth) do |value|
-          self[meth.slice(0..-2)] = value
+          write_property(method, value)
         end
       end
+    end
 
+    def generate_reader_method(method)
+      eigenclass = class << self; self; end
       eigenclass.class_eval do
-        define_method(meth) do |*args|
-          value = args[0]
-          if self.has_key?(meth)
-            if self[meth].is_a?(Hash)
-              SettingsNode[self[meth]]
-            else
-              self[meth]
-            end
+        define_method(method) do |*args|
+          value = args.first
+          if value.nil?
+            read_property(method)
           else
-            self[meth] = value || SettingsNode.new
+            write_property(method, value)
           end
         end
       end
-
-      send(meth, value, &block) 
     end
 
-    def load_form_hash(hash)
-      hash.each do |k, value|
-        if value.is_a?(Hash)
-          load_form_hash(value)
-        else
-          generate_method(k ,value)
+    def write_property(name, value)
+      target[name] = value
+    end
+
+    def read_property(name)
+
+      if target[name].nil?
+        target[name] = Node.new
+      elsif target[name].is_a?(Hash)
+        target[name] = build_hash(target[name])
+      else
+        target[name]
+      end
+    end
+
+    def target
+      self
+    end
+
+    def build_hash(hash)
+      hash.each do |k, v|
+        if v.is_a?(Hash)
+          hash[k] = build_hash(v)
         end
+      end
+      Node[hash]
+    end
+
+    module ClassMethods
+
+      def set_store(store)
+        # @@target = store
+
+        class_eval %(
+
+          def target
+            send('#{store}')
+          end
+        )        
       end
     end
   end
 
-  class << self
-    SETTINGS_FILE = "#{Rails.root}/config/settings.yml"
+  class Node < Hash
+    IGNORE_METHOS = [ :method_missing, :class_variables_get=, :class_variables_get, :generate_accessor_methods, :generate_writer_method, :generate_reader_method, :target, :freeze, :===, :==, :<=>, :<, :<=, :>, :>=, :to_s, :inspect, :included_modules, :include?, :ancestors, :instance_methods, :public_instance_methods, :protected_instance_methods, :private_instance_methods, :constants, :const_get, :const_set, :const_defined?, :const_missing, :class_variables, :remove_class_variable, :class_variable_get, :class_variable_set, :class_variable_defined?, :public_constant, :private_constant, :module_exec, :class_exec, :module_eval, :class_eval, :method_defined?, :public_method_defined?, :private_method_defined?, :protected_method_defined?, :public_class_method, :private_class_method, :autoload, :autoload?, :instance_method, :public_instance_method, :psych_yaml_as, :yaml_as, :psych_to_yaml, :to_yaml, :to_yaml_properties, :nil?, :=~, :!~, :eql?, :hash, :class, :singleton_class, :clone, :dup, :taint, :tainted?, :untaint, :untrust, :untrusted?, :trust, :frozen?, :methods, :singleton_methods, :protected_methods, :private_methods, :public_methods, :instance_variables, :instance_variable_get, :instance_variable_set, :instance_variable_defined?, :remove_instance_variable, :instance_of?, :kind_of?, :is_a?, :tap, :send, :public_send, :respond_to?, :extend, :display, :method, :public_method, :define_singleton_method, :object_id, :to_enum, :to_ary, :enum_for, :equal?, :!, :!=, :instance_eval, :instance_exec, :__send__, :__id__]
 
-    @@settings = SettingsNode.new
+    include GenerateAccessorMethods
 
     def method_missing(meth, *args, &block)
-      puts meth, *args
-      meth = meth.to_s
-      eigenclass = class << self; self; end
-
-      if meth[-1] == '=' # write
-        eigenclass.class_eval do
-          define_method(meth) do |value|
-            @@settings[meth.slice(0..-2)] = value
-          end
-        end
+      if !IGNORE_METHOS.include?(meth)
+        method = meth.to_s.chomp('=')
+        generate_accessor_methods(method)
+        send(meth, *args, &block)
       else
-        eigenclass.class_eval do
-          define_method(meth) do |*args|
-            value = args[0]
-            if @@settings.has_key?(meth)
-              if @@settings[meth].is_a?(Hash)
-                SettingsNode[@@settings[meth]]
-              else
-                @@settings[meth]
-              end
-            else
-              @@settings[meth] = value || SettingsNode.new
-            end            
-          end
-        end
+        super
       end
-      send(meth, *args, &block)     
     end
 
-    def values
-      @@settings
+    def class
+      Hash
+    end
+  end
+
+  class << self
+    IGNORE_METHOS = [:settings, :settings=, :config_file, :method_missing, :load, :save, :env, :all_settings, :class_variables_get=, :class_variables_get, :generate_accessor_methods, :generate_writer_method, :generate_reader_method, :target, :freeze, :===, :==, :<=>, :<, :<=, :>, :>=, :to_s, :inspect, :included_modules, :include?, :name, :ancestors, :instance_methods, :public_instance_methods, :protected_instance_methods, :private_instance_methods, :constants, :const_get, :const_set, :const_defined?, :const_missing, :class_variables, :remove_class_variable, :class_variable_get, :class_variable_set, :class_variable_defined?, :public_constant, :private_constant, :module_exec, :class_exec, :module_eval, :class_eval, :method_defined?, :public_method_defined?, :private_method_defined?, :protected_method_defined?, :public_class_method, :private_class_method, :autoload, :autoload?, :instance_method, :public_instance_method, :psych_yaml_as, :yaml_as, :psych_to_yaml, :to_yaml, :to_yaml_properties, :nil?, :=~, :!~, :eql?, :hash, :class, :singleton_class, :clone, :dup, :taint, :tainted?, :untaint, :untrust, :untrusted?, :trust, :frozen?, :methods, :singleton_methods, :protected_methods, :private_methods, :public_methods, :instance_variables, :instance_variable_get, :instance_variable_set, :instance_variable_defined?, :remove_instance_variable, :instance_of?, :kind_of?, :is_a?, :tap, :send, :public_send, :respond_to?, :extend, :display, :method, :public_method, :define_singleton_method, :object_id, :to_enum, :to_ary, :enum_for, :equal?, :!, :!=, :instance_eval, :instance_exec, :__send__, :__id__]
+
+    include GenerateAccessorMethods
+
+    attr_accessor :settings
+
+    set_store :settings
+
+    if defined?(Rails)
+      @@config_file  = "#{Rails.root}/config/settings.yml"
+    end
+
+    def config_file(file)
+      @@config_file = file 
+    end
+
+    def settings
+      @settings ||= Node.new
+    end
+
+    def method_missing(meth, *args, &block)
+      if !IGNORE_METHOS.include?(meth)
+        method =  meth.to_s.chomp('=')
+        generate_accessor_methods(method)
+        send(meth, *args, &block)     
+      else
+        super
+      end
     end
 
     def load
       @all_settings = nil
-      @@settings = all_settings[env]
+      @settings = all_settings[env]
     end
 
     def save
-      all_settings[env] = @@settings
-      File.open(SETTINGS_FILE, 'w+') {|f| f.write all_settings.to_yaml } 
+      all_settings[env] = @settings
+      File.open( @@config_file, 'w+' ) {|f| f.write all_settings.to_yaml } 
     end   
 
-    # protected
+    protected
 
-    def env 
-      Rails.env.to_s      
+    def env
+      ENV["RAILS_ENV"] || "development"
     end
 
     def all_settings
-      @all_settings ||= YAML::load_file(SETTINGS_FILE) 
+      @all_settings ||= YAML::load_file( @@config_file ) 
     rescue Errno::ENOENT
       @all_settings = {}
     end
--- spec/settings_spec.rb
+++ spec/settings_spec.rb
@@ -1,5 +1,90 @@
+ENV["RAILS_ENV"] ||= "test"
+
 require "settings"
+require "fileutils"
+
+Settings.config_file File.expand_path("../config/settings.yml", __FILE__)
 
 describe Settings do 
-	
+
+  describe "node access" do 
+    it "set a value" do 
+      Settings.test = 1
+      Settings.test.should == 1
+    end
+
+    it "get a value" do 
+
+      Settings.settings["hi"] = "World"
+      Settings.hi.should == "World"
+    end
+
+    it "set a high level value" do 
+      Settings.this.a.data = "1"
+      Settings.this.a.data.should == "1"
+    end
+
+    it "change a high level value" do 
+      Settings.that.a.data
+      Settings.that.a.data = 'hi'
+      Settings.that.a.data.should == 'hi'
+    end
+
+    it "change multi high level value" do 
+      Settings.that.a.data
+      Settings.that.a.data = 'hi'
+      Settings.that.a.data.should == 'hi'
+      Settings.that.a.name = 'hysios'
+      Settings.that.a.age = 18
+      Settings.that.a.name.should == 'hysios'
+      Settings.that.a.age.should == 18
+    end    
+  end
+
+  describe "load and save" do 
+    before :each do 
+      FileUtils.mkdir_p File.expand_path("../../tmp", __FILE__)
+      Settings.config_file File.expand_path("../config/data.yml", __FILE__)
+    end
+
+    let(:temp_file) {
+      File.expand_path("../../tmp/temp.yml", __FILE__)
+    }
+
+    it "save" do 
+      Settings.config_file temp_file
+      Settings.ready = "asdfasdf"
+      Settings.save
+
+      hash = YAML.load_file(temp_file)[ENV["RAILS_ENV"]]
+      hash.should include("ready" => "asdfasdf")
+      # Settings.save
+    end
+
+    it "load" do 
+      Settings.load
+      Settings.loading.from.file.should == 1234
+    end
+
+    it "load and set" do 
+      Settings.load
+      Settings.loading.from.file.should == 1234
+      Settings.loading.from.disk = 'A'
+      Settings.loading.from.should include("file" => 1234, "disk" => "A")
+    end    
+
+    it "load and save" do 
+      Settings.load
+      Settings.loading.from.file.should == 1234
+      Settings.loading.from.disk = 'A'
+      Settings.config_file temp_file
+      Settings.save
+
+      hash = YAML.load_file(temp_file)[ENV["RAILS_ENV"]]
+      hash.should include("loading")
+      from = hash["loading"]["from"]
+      from.should include("file" => 1234, "disk" => "A")
+
+    end    
+  end
 end
